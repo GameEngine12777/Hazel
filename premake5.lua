f38f14d9fc398f44581f18e32d86aee20bff916e
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2025-03-25 21:35:49 +0800

    fix: remove submodule GLFW.

--- premake5.lua
+++ premake5.lua
@@ -10,11 +10,6 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
--- Include directories relative to root folder (solution directory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
-
-include "Hazel/vendor/GLFW"
 
 project "Hazel"
 	location "Hazel"
@@ -36,14 +31,7 @@ project "Hazel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
-	}
-
-	links 
-	{ 
-		"GLFW",
-		"opengl32.lib"
+		"%{prj.name}/vendor/spdlog/include"
 	}
 
 	filter "system:windows"
