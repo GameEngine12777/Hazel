495921cf29fa0431cf472df01d52d80cee811ba3
JiangXin12777 <2649340476@qq.com>
2025-03-25 16:14:30 +0800

    feat: Added precompiled headers

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdio.h>
+
 #include "Log.h"
 
 #ifdef HZ_PLATFORM_WINDOWS
@@ -8,11 +8,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	 Hazel::Log::Init();
+	Hazel::Log::Init();
 
-	 HZ_CORE_WARNING("Initialize log!");
+	HZ_CORE_WARNING("Initialize log!");
 
-	 HZ_INFO("Hello! Var={0}", argc);
+	HZ_INFO("Hello! Var={0}", argc);
 
 	printf("Hazel Engine Start!\n");
 	auto app = Hazel::CreateApplication();
