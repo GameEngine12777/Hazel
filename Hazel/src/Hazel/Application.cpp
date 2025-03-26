086ab1e0e76a42a5749e66e2e840bd2bdf49faba
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2025-03-26 15:08:57 +0800

    feat: Add windows class and Render window.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,8 +3,11 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
+#include <GLFW/glfw3.h>
+
 Hazel::Application::Application()
 {
+	m_Window = std::unique_ptr<Window>(Window::Create());
 }
 
 Hazel::Application::~Application()
@@ -23,7 +26,12 @@ void Hazel::Application::Run()
 		HZ_TRACE(e.ToString());
 	}
 
-	while (true);
+	while (m_Running)
+	{
+		glClearColor(1, 0, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		m_Window->OnUpdate();
+	}
 }
 
 Hazel::Application* Hazel::CreateApplication()
