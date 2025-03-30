464e1396ab5349ba3a5ad398f9595d800034db0e
JiangXin12777 <2649340476@qq.com>
2025-03-30 17:24:47 +0800

    feat: Added input polling.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,6 +2,8 @@
 #include "Application.h"
 #include "Hazel/Log.h"
 
+#include "Input.h"
+
 #include <glad/glad.h>
 
 namespace Hazel
@@ -36,6 +38,9 @@ namespace Hazel
 			*/
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			// 更新视口
 			m_Window->OnUpdate();
