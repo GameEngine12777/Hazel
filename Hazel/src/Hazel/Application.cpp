e959db659ec7632dd54d5c0356bdc25deb8bd0f1
JiangXin12777 <2649340476@qq.com>
2025-04-03 20:04:17 +0800

    feat: Our first triangle.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -26,6 +26,8 @@ namespace Hazel
 		// 创建 imgui 图层
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		
 	}
 
 	Application::~Application()
@@ -38,6 +40,35 @@ namespace Hazel
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			/**
+			* 创建 VAO
+			*/
+			glGenVertexArrays(1, &m_VertexArray);
+			glBindVertexArray(m_VertexArray);
+
+			glGenBuffers(1, &m_VertexBuffer);
+			glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+			float vertices[3 * 3] = {
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.0f,  0.5f, 0.0f
+			};
+
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+			glGenBuffers(1, &m_IndexBuffer);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+			unsigned int indices[3] = { 0, 1, 2 };
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			/**
 			* 更新图层
--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,9 +17,16 @@ namespace Hazel {
 
 	void OpenGLContext::Init()
 	{
+		// m_WindowHandle（一个 GLFW 窗口对象）成为当前线程的 OpenGL 上下文。
 		glfwMakeContextCurrent(m_WindowHandle);
+		// 加载 OpenGL 函数
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		HZ_CORE_INFO("OpenGL Info:");
+		HZ_CORE_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
+		HZ_CORE_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
