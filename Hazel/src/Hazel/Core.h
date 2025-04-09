ec557f5bbae4c6fe9c43bceae772f3d6b1b45adb
JiangXin12777 <2649340476@qq.com>
2025-04-09 16:41:36 +0800

    feat: Added Hazel::Scope and Hazel::Ref definitions.

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -29,3 +29,13 @@
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(Fun) std::bind(Fun, this, std::placeholders::_1)
+
+namespace Hazel {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
