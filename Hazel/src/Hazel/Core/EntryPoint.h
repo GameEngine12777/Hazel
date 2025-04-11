f9de555f56111f464a41f129521666b29d5261e1
JiangXin12777 <2649340476@qq.com>
2025-04-11 20:03:53 +0800

    feat: Initial instrumentation work.

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -8,11 +8,18 @@ int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
 
-	HZ_CORE_WARNING("Hello, Welcome to the Game Engine!!!");
-
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
 	auto app = Hazel::CreateApplication();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
 	app->Run();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Shutdown.json");
 	delete app;
+	HZ_PROFILE_END_SESSION();
+
 	return 0;
 }
 
