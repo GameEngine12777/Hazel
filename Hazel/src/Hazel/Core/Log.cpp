aeca52dd5d751a04957be149ba651378818c35ca
JiangXin12777 <2649340476@qq.com>
2025-04-12 15:35:28 +0800

    feat: Log messages are now additionally sent to Hazel.log file.

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Hazel/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Hazel {
 	Ref<spdlog::logger> Log::s_CoreLogger = nullptr;
@@ -9,12 +10,26 @@ namespace Hazel {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%c] %n: %v%$");
+		std::vector<spdlog::sink_ptr> logSinks;
+		// 控制台日志输出
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 
-		s_CoreLogger = spdlog::stdout_color_mt("Hazel");
-		s_CoreLogger->set_level(spdlog::level::level_enum::trace);
+		// 文件日志输出
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
 
-		s_ClientLogger = spdlog::stdout_color_mt("App");
-		s_ClientLogger->set_level(spdlog::level::level_enum::trace);
+		// 设置输出格式
+		logSinks[0]->set_pattern("%^[%c] [%l] %n: %v%$");
+		logSinks[1]->set_pattern("[%c] [%l] %n: %v");
+
+		// 同时向控制台和文件输出
+		s_CoreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
