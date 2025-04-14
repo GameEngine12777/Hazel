7ae659cf1fe6c7dafb337bc09b8b5ae6165c69cd
JiangXin12777 <2649340476@qq.com>
2025-04-14 10:29:12 +0800

    fix: General cleanup.

--- Hazel/src/Hazel/Core/Application.cpp
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -1,17 +1,14 @@
 #include "hzpch.h"
 #include "Hazel/Core/Application.h"
 #include "Hazel/Core/Log.h"
-
-#include "Hazel/ImGui/ImGuiLayer.h"
-
 #include "Hazel/Core/Input.h"
-
+#include "Hazel/Core/KeyCodes.h"
+#include "Hazel/ImGui/ImGuiLayer.h"
 #include "Hazel/Renderer/Renderer.h"
-
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/VertexArray.h"
-#include "Hazel/Core/KeyCodes.h"
+
 #include <GLFW/glfw3.h>
 
 namespace Hazel
--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -1,7 +1,7 @@
 #include "hzpch.h"
-#include <glad/glad.h>
-
 #include "Platform/OpenGL/OpenGLRendererAPI.h"
+
+#include <glad/glad.h>
 
 namespace Hazel {
 
