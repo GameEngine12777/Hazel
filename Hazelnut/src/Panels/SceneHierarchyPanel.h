457ab91e2ff6a8b871cb3371bdfe9d2474c68e60
JiangXin12777 <2649340476@qq.com>
2025-04-16 11:18:13 +0800

    feat: Added Scene Hierarchy panel.

---
+++ Hazelnut/src/Panels/SceneHierarchyPanel.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Hazel/Core/Base.h"
+#include "Hazel/Core/Log.h"
+#include "Hazel/Scene/Scene.h"
+#include "Hazel/Scene/Entity.h"
+
+namespace Hazel {
+
+	/**
+	* 场景信息面板
+	* 场景大纲 ？
+	*/
+	class SceneHierarchyPanel
+	{
+	public:
+		SceneHierarchyPanel() = default;
+		SceneHierarchyPanel(const Ref<Scene>& scene);
+
+		void SetContext(const Ref<Scene>& scene);
+
+		void OnImGuiRender();
+	private:
+		void DrawEntityNode(Entity entity);
+	private:
+		Ref<Scene> m_Context;
+		Entity m_SelectionContext;
+	};
+
+}
