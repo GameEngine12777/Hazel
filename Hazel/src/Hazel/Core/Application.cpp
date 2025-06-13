db1d5216e31806f5906d98de8a7a2a9b45b042fd
JiangXin12777 <2649340476@qq.com>
2025-06-13 20:29:52 +0800

    fix: Implemented platform agnostic GetTime() (#556)
    -Provides a way to just import platform utils and get time in seconds by calling Time::GetTime(), without hard coding the glfw way of obtaining it.

--- Hazel/src/Hazel/Core/Application.cpp
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -8,8 +8,7 @@
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/VertexArray.h"
-
-#include <GLFW/glfw3.h>
+#include "Hazel/Utils/PlatformUtils.h"
 
 namespace Hazel
 {
@@ -51,7 +50,7 @@ namespace Hazel
 		{
 			HZ_PROFILE_SCOPE("RunLoop");
 
-			float time = (float)glfwGetTime();
+			float time = Time::GetTime();
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
--- Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -58,4 +58,8 @@ namespace Hazel {
 		return std::string();
 	}
 
+	float Time::GetTime()
+	{
+		return glfwGetTime();
+	}
 }
